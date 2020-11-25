59d735b4f7078bcd46047372ea4e976a2ca8eb48
Prabu <meetprabu88@gmail.com>
2020-11-25 15:00:00 +0530

    Sample app with multiple pages

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { Page, Layout, EmptyState } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+const Index = () => (
+  <div>
+  	<Page>
+  		<Layout>
+        <EmptyState
+	        heading="Discount your products temporarily"
+	        action={{
+	          content: 'Select products',
+	          onAction: () => console.log('clicked'),
+	        }}
+	        image={img}
+	      >
+	        <p>Select products to change their price temporarily.</p>
+	      </EmptyState>
+	  	</Layout>
+  	</Page>
+  </div>
+);
+
+export default Index;
